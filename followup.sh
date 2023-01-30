6d84f478892a0eee697c823142a213098bfc58cd
Gordon Keller <gordon.h.keller@gmail.com>
2023-01-29 16:12:32 -0800

    generalized files/directories referenced in script (namely, the attachments directory and the client csv name)

--- followup.sh
+++ followup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-notesdir="notes"
+echo "now running ~ followup ~"
+echo "written by Gordon Keller"
+echo "created: 1/29/23"
+echo "modified: 1/29/23"
+sleep 1
+
+# parse out the test flag (expect '-t')
+client_csv=$1
+attch_dir=$2
 
 i=1
 while IFS=, read -r cfn cln sfn sln email phone inst
@@ -12,6 +20,18 @@ do
     # skip empty lines
     if [ -z "$email" ]; then continue; fi
 
+    echo ""
+    echo "-----------------------------------"
+    echo "Starting a new job..."
+
+    # print out the current job
+    echo "Client: $cfn $cln"
+    echo "Student: $sfn $sln"
+    echo "Email address: $email"
+    echo "Phone number: $phone"
+    echo "Instrument: $inst"
+
+
     # check to see if student has a new note
     # returns a note path if it exists
     # returns no note path if not
@@ -19,7 +39,7 @@ do
     student=""
     date=""
     note_path=""
-    for FILE in $notesdir/*; do 
+    for FILE in $attch_dir/*; do 
         # parse out last part of file
         note_file_name=$(basename "$FILE")
         nfn_front="${note_file_name%.*}"
@@ -31,7 +51,7 @@ do
         # copy the path name to the output path and proceed
         if [ ${student,,} == ${sfn,,} ]; then
             note_path=$FILE
-            echo "attach -> $note_path"
+            echo "Found note for $sfn -- attach to email -> $note_path"
         fi
     done
 
@@ -44,7 +64,7 @@ do
         # check for client-is-the-student case
         if [ ${cfn,,} == ${sfn,,} ]
             then
-                echo "Hi $sfn, here are the notes from your lesson this week. Thanks! - Gordon" | \
+                echo "Hi $sfn, here are the notes from your $inst lesson this week. Thanks! - Gordon" | \
                     mutt -s "$sfn's Lesson Notes - $date" \
                     $email \
                     -a $note_path --
@@ -56,4 +76,11 @@ do
         fi
     fi
 
-done < clients.csv
+    echo "Done with active job for $cln,$cfn."
+    echo "-----------------------------------"
+    echo ""
+
+    sleep 1
+
+done < $client_csv
+echo "Lesson Follow-up Email Script finished!"
